bf9a4b1e97448f3f197bb3831375feef9bc869fd
Celso Mothé <celso_mothe@hotmail.com>
2022-05-04 16:01:49 -0300

    alterando o arquivo principal

--- src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
+++ src/main/java/com/dio/santander/bankline/api/BanklineApiApplication.java
@@ -8,6 +8,7 @@ public class BanklineApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BanklineApiApplication.class, args);
+		//Iremos começar a codar o nosso projeto.
 	}
 
 }
